e85b1a97e414f00cb21c2dff5cae6dd10b668110
Fábio Filho <fabio.filhossi@gmail.com>
2017-04-14 03:18:37 -0300

    Changing the front-end: implementing the boostrap framework.

--- AppCrawler/Robot.GooglePlay/Helpers/GooglePlayUtils.cs
+++ AppCrawler/Robot.GooglePlay/Helpers/GooglePlayUtils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Robot.GooglePlay.Helpers
@@ -17,6 +18,16 @@ namespace Robot.GooglePlay.Helpers
         }
 
         private static string FixRating(string rating) {
+
+            if (rating == string.Empty)
+                return string.Empty;
+
+            rating = rating.Trim();
+            if (rating.Length > 5)
+                rating = rating.Substring(0, 5);
+
+            rating = rating.Replace(",", ".");
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
             double result = Convert.ToDouble(rating);
             result = Math.Round((result * 5) / 100, 1);
