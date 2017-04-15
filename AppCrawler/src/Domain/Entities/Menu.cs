6ad28dd41634ddd377c6f8a9813d6b7297d56692
Fábio Filho <fabio.filhossi@gmail.com>
2017-04-15 03:26:40 -0300

    Created the Menu class and finished the _Layout.cshtml .

---
+++ AppCrawler/src/Domain/Entities/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public static class Menu
+    {
+         public static readonly string[,] Itens = {
+            { "Home,Index,Details", "/Home/Index" },
+            { "Contact", "/Home/Contact" },
+            { "About", "/Home/About" },
+            { "GitHub", "https://github.com/vanhackathon-team/app-samurai"}
+        };
+
+        public static string GetMenuName(int index) {
+            return Itens[index,0].Split(',')[0];
+        }
+
+        public static string GetStatus(string currentAction, int index) {
+            
+           foreach (string action in Itens[index, 0].Split(',')) 
+           if (currentAction.ToLower().Contains(action.ToLower()))
+                return "active";                                          
+
+            return "";
+        }
+
+    }
+}
